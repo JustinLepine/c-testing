77e895401eed4ddc4809497047f4e7c508ba6b85
Justin Lepine <lepine.justin@yahoo.com>
2022-06-07 11:05:09 -0400

    testing c read write line

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WindowHeight = 40;
+Console.Title = "Ramen";
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Hello whats your name?");
+Console.ReadLine();
+Console.WriteLine("My Name is Takumi");
+Console.WriteLine("Whats your favourite car?");
+Console.ReadLine();
+Console.WriteLine("Nice that's sick");
+
+Console.ReadKey();
